4dfc99f343f9f928c07e66dfb72039a0f24f5260
Luigi9411 <luigi.sciavicco@gmail.com>
2022-10-24 19:43:30 +0200

    add-html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-for (let i = 1; i <=100; i++){
-    if (i % 3==0 && i % 5==0){
-       console.log('FizzBuzz')
-    }else if(i%3==0){ 
-       console.log('Fizz')
-    }else if(i%5==0){
-        console.log('Buzz')
+
+ for (let i = 1; i <=100; i++){
+     if (i % 3==0 && i % 5==0){
+        console.log('FizzBuzz')
+     }else if(i%3==0){ 
+        console.log('Fizz')
+     }else if(i%5==0){
+         console.log('Buzz')
+     }
+         console.log (i)
+     }
+    
+    const eleSquares = document.querySelector('.squares');
+
+    for (let i = 1; i <=100; i++){
+
+        if (i % 3==0 && i % 5==0){
+           eleSquares.innerHTML += `<div class="sq4">FizzBuzz</div>`;
+        }else if(i%3==0){ 
+           eleSquares.innerHTML += `<div class="sq2">Fizz</div>`;
+        }else if(i%5==0){
+            eleSquares.innerHTML += `<div class="sq3">Buzz</div>`;
+        }
+        else{
+            eleSquares.innerHTML += `<div class="sq">${i}</div>`;
+        }
+
     }
-        console.log (i)
-    }
+    
